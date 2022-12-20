c10dd42503e34e4aa819c8ea0c8fb8e378933c5e
ndikumana <ndikumanaisaie@gmail.com>
2022-12-20 07:08:27 +0100

    Add student class

--- person.rb
+++ person.rb
@@ -1,24 +1,25 @@
 class Person
   attr_accessor :name, :age
   attr_reader :id
-  def initialize(age, name = 'Unknown', parent_permission = true)
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
-  private :is_of_age?
+  private :of_age?
 end
 
-person1 = Person.new(34, "ndikumana")
+person1 = Person.new(34, 'ndikumana')
 
-puts person1.name();
+puts person1.name
