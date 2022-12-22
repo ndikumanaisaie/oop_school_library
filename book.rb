d05d8717ec52bb40da430fbdc7dc46e6f87a83fb
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2022-12-22 14:27:38 +0100

    Add book class

--- book.rb
+++ book.rb
@@ -1,2 +1,16 @@
+require_relative 'rental'
+
 class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    new_rental = Rental.new(date, self, person)
+    @rentals.push(new_rental)
+  end
 end
