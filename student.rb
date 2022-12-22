635ca4e6ba3a1bb8e50848258b2e125bcd8287b2
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2022-12-22 15:51:58 +0100

    Fix linter error

--- student.rb
+++ student.rb
@@ -9,6 +9,7 @@ class Student < Person
   def play_hooks
     '¯(ツ)/¯'
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
