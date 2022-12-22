0ce963e07fa55b09c2986ed7c0e44c92e435a718
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2022-12-22 23:58:02 +0100

    Create person feature

--- app.rb
+++ app.rb
@@ -15,16 +15,27 @@ class App
 
   def list_all_books
     puts 'There are no books yet! Please add books.' if @books.empty?
-
     @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     sleep 0.75
-    menu
   end
 
   def list_all_people
     puts 'There are no people yet! Please add a student or teacher.' if @people.empty?
     @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
     sleep 0.75
-    menu
+  end
+
+  def create_a_person
+    print 'Do you want to create a student (1) or teacher (2) [Input a number]: '
+    option = gets.chomp
+
+    case option
+    when '1'
+      create_a_student
+    when '2'
+      create_a_teacher
+    else
+      puts 'Invalid input. Please type 1 or 2'
+    end
   end
 end
