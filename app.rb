bd05cec4504f70f28044b1a1895c33bcf39f400a
Ndikumana Isaie <ndikumanaisaie@gmail.com>
2022-12-22 23:55:24 +0100

    Add book and people

--- app.rb
+++ app.rb
@@ -4,3 +4,27 @@ require_relative 'teacher'
 require_relative 'rental'
 require_relative 'book'
 require_relative 'classroom'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+    @class = Classroom.new('Grade 5')
+  end
+
+  def list_all_books
+    puts 'There are no books yet! Please add books.' if @books.empty?
+
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    sleep 0.75
+    menu
+  end
+
+  def list_all_people
+    puts 'There are no people yet! Please add a student or teacher.' if @people.empty?
+    @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    sleep 0.75
+    menu
+  end
+end
